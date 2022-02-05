ee01638dd2f42b7e1bad3193de1a016ac035ed60
Dmitriy <dimryb@bk.ru>
2022-02-05 12:29:06 +0700

    Добавил проект с тестовым рекламным баннером.

---
+++ app/src/main/java/space/rybakov/earnapp/MainActivity.kt
@@ -0,0 +1,40 @@
+package space.rybakov.earnapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.MobileAds
+import space.rybakov.earnapp.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initAdMob()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.adView.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.adView.pause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.adView.destroy()
+    }
+
+    private fun initAdMob(){
+        MobileAds.initialize(this)
+        val adRequest = AdRequest.Builder().build()
+        binding.adView.loadAd(adRequest)
+    }
+}
